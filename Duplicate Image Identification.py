7c36e1e66eefcd9384b0eb538604955a7451a577
Bikash12 <bikash8181@gmail.com>
2020-03-29 13:40:30 +0530

    Using pickle file

--- Duplicate Image Identification.py
+++ Duplicate Image Identification.py
@@ -2,6 +2,86 @@
 from pandas import read_csv
 
 tagged = dict([(p,w) for _,p,w in read_csv('C:/Users/bikas/PycharmProjects/Humpback_Whale_Identification-/train.csv').to_records()])
-submit = [p for _,p,_ in read_csv('../input/whale-categorization-playground/sample_submission.csv').to_records()]
-join   = list(tagged.keys()) + submit
+submit = [p for _,p,_ in read_csv('C:/Users/bikas/PycharmProjects/Humpback_Whale_Identification-/sample_submission.csv').to_records()]
+join = list(tagged.keys()) + submit
 len(tagged),len(submit),len(join),list(tagged.items())[:5],submit[:5]
+
+# Determise the size of each image
+from os.path import isfile
+from PIL import Image as pil_image
+from tqdm import tqdm_notebook
+
+def expand_path(p):
+    if isfile('C:/Users/bikas/PycharmProjects/Humpback_Whale_Identification-/train/' + p): return 'C:/Users/bikas/PycharmProjects/Humpback_Whale_Identification-/train/' + p
+    if isfile('C:/Users/bikas/PycharmProjects/Humpback_Whale_Identification-/test/' + p): return 'C:/Users/bikas/PycharmProjects/Humpback_Whale_Identification-/test/' + p
+    return p
+
+p2size = {}
+for p in tqdm_notebook(join):
+    size      = pil_image.open(expand_path(p)).size
+    p2size[p] = size
+len(p2size), list(p2size.items())[:5]
+
+# Read or generate p2h, a dictionary of image name to image id (picture to hash)
+import pickle
+import numpy as np
+from imagehash import phash
+from math import sqrt
+
+# Two phash values are considered duplicate if, for all associated image pairs:
+# 1) They have the same mode and size;
+# 2) After normalizing the pixel to zero mean and variance 1.0, the mean square error does not exceed 0.1
+def match(h1,h2):
+    for p1 in h2ps[h1]:
+        for p2 in h2ps[h2]:
+            i1 =  pil_image.open(expand_path(p1))
+            i2 =  pil_image.open(expand_path(p2))
+            if i1.mode != i2.mode or i1.size != i2.size: return False
+            a1 = np.array(i1)
+            a1 = a1 - a1.mean()
+            a1 = a1/sqrt((a1**2).mean())
+            a2 = np.array(i2)
+            a2 = a2 - a2.mean()
+            a2 = a2/sqrt((a2**2).mean())
+            a  = ((a1 - a2)**2).mean()
+            if a > 0.1: return False
+    return True
+
+if isfile('C:/Users/bikas/PycharmProjects\Humpback_Whale_Identification-/p2h.pickle'):
+    with open('C:/Users/bikas/PycharmProjects\Humpback_Whale_Identification-/p2h.pickle', 'rb') as f:
+        p2h = pickle.load(f)
+else:
+    # Compute phash for each image in the training and test set.
+    p2h = {}
+    for p in tqdm_notebook(join):
+        img    = pil_image.open(expand_path(p))
+        h      = phash(img)
+        p2h[p] = h
+
+    # Find all images associated with a given phash value.
+    h2ps = {}
+    for p,h in p2h.items():
+        if h not in h2ps: h2ps[h] = []
+        if p not in h2ps[h]: h2ps[h].append(p)
+
+    # Find all distinct phash values
+    hs = list(h2ps.keys())
+
+    # If the images are close enough, associate the two phash values (this is the slow part: n^2 algorithm)
+    h2h = {}
+    for i,h1 in enumerate(tqdm_notebook(hs)):
+        for h2 in hs[:i]:
+            if h1-h2 <= 6 and match(h1, h2):
+                s1 = str(h1)
+                s2 = str(h2)
+                if s1 < s2: s1,s2 = s2,s1
+                h2h[s1] = s2
+
+    # Group together images with equivalent phash, and replace by string format of phash (faster and more readable)
+    for p,h in p2h.items():
+        h = str(h)
+        if h in h2h: h = h2h[h]
+        p2h[p] = h
+
+len(p2h), list(p2h.items())[:5]
+
